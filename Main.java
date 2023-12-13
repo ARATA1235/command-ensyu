5b6959e54c1b6a7ee8d84e48553712f161626071
Arata <a.sasaki.sys23@morijyobi.ac.jp>
2023-12-13 15:22:51 +0900

    二人分のBMI

--- Main.java
+++ Main.java
@@ -6,10 +6,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         double height = Double.parseDouble(sc.nextLine());
         double weight = Double.parseDouble(sc.nextLine());
+    	
+    	double height2 = Double.parseDouble(sc.nextLine());
+        double weight2 = Double.parseDouble(sc.nextLine());
 
         double BMI = weight / (height * height);
         String bmi = String.format("%.2f", BMI);
+    	
+    	double BMI2 = weight2 / (height2 * height2);
+        String bmi = String.format("%.2f", BMI2);
 
-        System.out.println("あなたのBMIは" + bmi + "です。");
+        System.out.println("一人目のBMIは" + bmi + "です。");
+    	
+    	System.out.println("二人目のBMIは" + bmi + "です。");
     }
 }
