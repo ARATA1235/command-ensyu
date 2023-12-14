b686628a160b0518a32944ca07f17641861eaefc
Arata <a.sasaki.sys23@morijyobi.ac.jp>
2023-12-14 09:47:11 +0900

    コンパイル完了

--- Bmi.java
+++ Bmi.java
@@ -6,18 +6,20 @@ public class Bmi {
         Scanner sc = new Scanner(System.in);
         double height = Double.parseDouble(sc.nextLine());
         double weight = Double.parseDouble(sc.nextLine());
-    	
-    	double height2 = Double.parseDouble(sc.nextLine());
+
+        System.out.println("身長(m)、体重(㎏)を表記してください。");
+
+        double height2 = Double.parseDouble(sc.nextLine());
         double weight2 = Double.parseDouble(sc.nextLine());
 
         double BMI = weight / (height * height);
         String bmi = String.format("%.2f", BMI);
-    	
-    	double BMI2 = weight2 / (height2 * height2);
-        String bmi = String.format("%.2f", BMI2);
+
+        double BMI2 = weight2 / (height2 * height2);
+        String bmi2 = String.format("%.2f", BMI2);
 
         System.out.println("一人目のBMIは" + bmi + "です。");
-    	
-    	System.out.println("二人目のBMIは" + bmi + "です。");
+
+        System.out.println("二人目のBMIは" + bmi2 + "です。");
     }
 }
